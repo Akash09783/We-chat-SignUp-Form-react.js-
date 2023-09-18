a96b29253e0baeb5a9e6acf903f5f2245cecff55
Akash <kumawat.aakash515@gmail.com>
2023-09-18 22:17:25 +0530

    working on logics

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+
+import React,{useState} from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Logo from "../assets/logo.svg"
-function Register() {
+import Logo from "../assets/logo.svg";
+import { toast,ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export default function Register() {
+    const toastOptions={
+        position:"bottom-right",
+        autoClose:8000,
+        pauseOnHover:true,
+        draggable:true,
+        theme:"dark", 
+    }
+  const [values, setValues] = useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+  });
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("form");
+    handleValidation();
   };
-  const handleChange = (event) =>{}; 
+const handleValidation=()=>{
+    const {password,confirmPassword,username,email}=values;
+    if(password!==confirmPassword){
+        toast.error("password and confirm password should be same.",
+        toastOptions
+        );
+        return false;
+    }else if(username.length<3){
+        toast.error("username should be greater than 3 characters",
+        toastOptions);
+        return false;
+
+    }else if (password.length < 8) {
+        toast.error(
+          "Password should be equal or greater than 8 characters.",
+          toastOptions
+        );
+        return false;
+    }
+    else if(email===""){
+        toast.error("Email is required",
+        toastOptions);
+        return false;
+}
+return true;
+};
+  const handleChange = (event) =>{
+    setValues({...values,[event.target.name]:event.target.value});
+  }; 
   return (
     <>
       <FormContainer>
@@ -44,6 +88,7 @@ function Register() {
           <span>Already have an account? <Link to="Login">Login</Link></span>
         </form>
       </FormContainer>
+      <ToastContainer/>
     </>
   );
 }
@@ -118,4 +163,4 @@ input{
 
 
 
-export default Register;
+
