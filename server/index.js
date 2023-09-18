a1f3ee0c3e4b43542b5c2cc972bb9d6a00aef3e7
Akash <kumawat.aakash515@gmail.com>
2023-09-18 16:14:47 +0530

    Server setup done

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require ("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>{
+    console.log("DB Connection Successfull");
+})
+.catch((err)=>{
+    console.log(err.message);
+
+});
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`server started on Port${process.env.PORT}`)})
+
+
+
+//     const mongoose = require ('mongoose');
+// const mongoURI ="mongodb://localhost:27017//chat"
+
+
+// const connectToMongo = ()=>{
+//     mongoose.connect(mongoURI, ()=>{
+//         console.log("connected to Mongo Succesfully");
+
+//     })
+// // }
+
+// module.exports = connectToMongo;
